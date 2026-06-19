712625b845f02195a0f0c8f04675f5720dff5fd0
yi0509 <924526470@qq.com>
2026-06-19 15:43:31 +0800

    学号+姓名 完成CI/CD页面修改

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>CI/CD 自动部署实验成功！服务器IP：8.138.122.7</h1>"
+    return "<h1>CI/CD自动部署实验<br>学号：2440666121<br>姓名：古心怡<br>服务器IP：8.138.122.7</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
